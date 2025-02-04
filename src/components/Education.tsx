c50b87a05a6ba380acb2159fad6eb9aacda17e41
konradhorber <konrad.horber@gmail.com>
2025-02-04 14:24:00 +0100

    Add education editing

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,6 +1,90 @@
+import { useState } from "react";
 
 function Education() {
+    const [isEditing, setIsEditing] = useState(true);
+    const [experiences, setExperiences] = useState([
+        { 
+        schoolName: "",
+        titleOfStudy: "",
+        graduationDate: ""
+        }
+    ]);
 
+    const handleExperienceChange = (index: number, e: React.ChangeEvent<HTMLInputElement>) => {
+        const updatedExperiences = experiences.map((experience, i) => 
+            i === index 
+                ? {...experience, [e.target.name]: e.target.value }
+                : experience
+        );
+        
+        setExperiences(updatedExperiences);
+    };
+
+    const addExperience = () => {
+        setExperiences([
+            ...experiences,
+            { 
+                schoolName: "",
+                titleOfStudy: "",
+                graduationDate: ""
+            }
+        ]);
+    }
+
+    const toggleEditing = () => {
+        setIsEditing(!isEditing);
+    };
+
+    return (
+        <form>
+            {isEditing ? (
+                <>
+                    {experiences.map((experience, index) => (
+                        <div key={index}>
+                            <label htmlFor="schoolName">School name:</label>
+                            <input 
+                                type="text" 
+                                id="schoolName" 
+                                name="schoolName"
+                                value={experience.schoolName}
+                                onChange={(e) => handleExperienceChange(index, e)}
+                            />
+                            <label htmlFor="titleOfStudy">Title of study:</label>
+                            <input 
+                                type="text"
+                                id="titleOfStudy" 
+                                name="titleOfStudy"
+                                value={experience.titleOfStudy}
+                                onChange={(e) => handleExperienceChange(index, e)}
+                            />
+                            <label htmlFor="graduationDate">Graduation date:</label>
+                            <input 
+                                type="date" 
+                                id="graduationDate" 
+                                name="graduationDate"
+                                value={experience.graduationDate}
+                                onChange={(e) => handleExperienceChange(index, e)}
+                            />
+                        </div>
+                    ))}
+                    <button type="button" onClick={addExperience}>
+                        Add experience
+                    </button>
+                </>
+            ) : (
+                experiences.map((experience, index) => (
+                    <div key={index}>
+                        <p>School name: {experience.schoolName as string}</p>
+                        <p>Title of study: {experience.titleOfStudy as string}</p>
+                        <p>Graduation date: {experience.graduationDate as string}</p>
+                    </div>
+                ))
+            )}
+            <button type="button" onClick={toggleEditing}>
+                Toggle editing
+            </button>
+        </form>
+    );
 }
 
-export default Education
+export default Education;
